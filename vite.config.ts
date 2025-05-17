d75b84d86e06a10560a4f049cc7ccabc23d9be22
princeghostme <coolkidzzz207@gmail.com>
2025-05-17 21:30:51 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,27 @@ export default defineConfig({
     tailwindcss(),
   ],
   base: "./",
+   build: {
+    target: 'esnext', // Ensures modern JS compatibility
+    minify: 'terser', // Uses Terser for minification\
+    terserOptions: {
+      compress: {
+        drop_console: true,  // Removes console.log, console.warn, console.error, etc.
+        drop_debugger: true, // Removes debugger statements
+      },
+      mangle: true, // Obfuscates variable names
+      format: {
+        comments: false, // Removes comments
+      },
+    },
+    assetsInlineLimit: 0, // Avoids inlining assets
+    rollupOptions: {
+      output: {
+        format: 'iife', // Wraps code into an Immediately Invoked Function Expression (IIFE)
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name][extname]',
+      },
+    },
+  }
 })
