88884cc0ca38dd347a262d5acb70d8dcb4041319
princeghostme <coolkidzzz207@gmail.com>
2025-05-18 21:21:21 +0530

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { TopNavBar } from './components/FloatingNavBar'
 import AppRouter from './routers/AppRouter'
 import ContactMe from './components/ContactMe '
@@ -16,7 +16,7 @@ const App = () => {
         {/* Copyright Footer */}
         <footer className="py-4 text-center text-sm text-gray-500">
           <div className="container mx-auto px-4">
-            <p>© {currentYear} All Rights Reserved. Designed and Developed with ❤️</p>
+            <p>© {currentYear} All Rights Reserved. Designed and Developed by Prince Yadav</p>
           </div>
         </footer>
       </Router>
