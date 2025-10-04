3e093a911997f0f9243e6ebcfd49b734136610e9
prince <coolkidzzz207@gmail.com>
2025-10-04 15:10:46 +0530

    fix: update LinkedIn URL to include protocol

--- src/AppConstraint.ts
+++ src/AppConstraint.ts
@@ -1,6 +1,6 @@
 
 export const URLS = {
-    LINKEDIN: "www.linkedin.com/in/prince-yadav-0b3267382",
+    LINKEDIN: "https://www.linkedin.com/in/prince-yadav-0b3267382",
     GITHUB: "https://github.com/princeghostme",
     WHATSAPP: "https://wa.me/917004571707",
 }
