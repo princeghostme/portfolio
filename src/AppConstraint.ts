df3976b5198c61830e52de8ae0c1a23a6947435c
prince <coolkidzzz207@gmail.com>
2025-10-04 15:46:58 +0530

    fix: move sensitive URLs to environment variables

--- src/AppConstraint.ts
+++ src/AppConstraint.ts
@@ -1,6 +1,6 @@
 
 export const URLS = {
-    LINKEDIN: "https://www.linkedin.com/in/prince-yadav-0b3267382",
-    GITHUB: "https://github.com/princeghostme",
-    WHATSAPP: "https://wa.me/917004571707",
+    LINKEDIN: process.env.LINKEDIN,
+    GITHUB: process.env.GITHUB,
+    WHATSAPP: process.env.WHATSAPP,
 }
